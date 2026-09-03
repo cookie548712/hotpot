68fd9a8fa349702b7edda41b52899f7204a6f79a
cclesule <14284106+cclesule@user.noreply.gitee.com>
2026-09-03 11:47:04 +0800

    feat: 名称更改

--- hotpot/src/config.js
+++ hotpot/src/config.js
@@ -1,7 +1,7 @@
 /** 专属称呼 —— 改这一处即可 */
-export const NICKNAME = '🎂'
+export const NICKNAME = '莲雾大王🍏'
 
 /** 小馆名，页面眉题 */
-export const BRAND = '今夜小馆'
+export const BRAND = '今日小食'
 
 export const STORAGE_KEY = 'hotpot-order-v1'
